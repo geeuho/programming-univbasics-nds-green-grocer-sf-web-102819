dc3ff31bfc4e9e14997de285446db6ef5402c7af
Allen Shin <geeuho@gmail.com>
2019-10-28 06:04:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,11 +31,27 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
 end
 
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+def apply_coupons(cart, coupons) 
+  
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+    
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
 end
+
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
